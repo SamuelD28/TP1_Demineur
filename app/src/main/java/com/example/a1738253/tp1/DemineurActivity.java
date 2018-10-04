02f03f9a90889bee87b35a47ed82c0f8d18003f4
DESKTOP-BKJJBIN\Sameul Dube <samuel_personnel@outlook.com>
2018-10-04 19:53:40 -0400

    Generation du tableau et des boutons

--- app/src/main/java/com/example/a1738253/tp1/DemineurActivity.java
+++ app/src/main/java/com/example/a1738253/tp1/DemineurActivity.java
@@ -1,43 +1,44 @@
 package com.example.a1738253.tp1;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.ViewTreeObserver;
 import android.widget.Button;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 
-import java.util.ArrayList;
-
 public class DemineurActivity extends AppCompatActivity {
 
-
-    private ArrayList<Button> bouttons = new ArrayList<Button>();
+    private TableLayout tableauDemineur;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_demineur);
 
-        TableLayout table = (TableLayout) findViewById(R.id.tableLayout);
+        tableauDemineur = findViewById(R.id.tableauDemineur);
+    }
 
-        for (int i = 0; i < 10; i++) {
+    //On utilise cette methode pour pouvoir obtenir la hauteur et la largeur du tableau.
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        GenererTableau(tableauDemineur.getHeight(), tableauDemineur.getWidth());
+    }
+
+    private void GenererTableau(int height, int width)
+    {
+
+        for (int i = 0; i < 9; i++) {
             TableRow rangee = new TableRow(this);
 
-            for (int o = 0; o < 10; o++) {
+            for (int o = 0; o < 9; o++) {
                 Button boutton = new Button(this);
-                rangee.addView(boutton,100,100);
-
+                rangee.addView(boutton,width/9,height/9);
             }
-            table.addView(rangee);
 
+            tableauDemineur.addView(rangee);
         }
-
-
     }
-    // bouttons.add(boutton);
-
-
-
-
-
 }
---
+++ app/src/main/java/com/example/a1738253/tp1/Mine.java
@@ -0,0 +1,5 @@
+package com.example.a1738253.tp1;
+
+public class Mine {
+
+}
