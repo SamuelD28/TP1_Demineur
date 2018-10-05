f32088179c179d0101fdc4e3c2635da0bc730f40
DESKTOP-BKJJBIN\Sameul Dube <samuel_personnel@outlook.com>
2018-10-04 21:06:20 -0400

    Generation et insertions des mines dans le tableau

--- app/src/main/java/com/example/a1738253/tp1/DemineurActivity.java
+++ app/src/main/java/com/example/a1738253/tp1/DemineurActivity.java
@@ -1,16 +1,25 @@
 package com.example.a1738253.tp1;
 
-import android.graphics.drawable.LayerDrawable;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.view.ViewTreeObserver;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Random;
 
 public class DemineurActivity extends AppCompatActivity {
 
     private TableLayout tableauDemineur;
+    private ArrayList<Mine> listeMine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,26 +27,50 @@ public class DemineurActivity extends AppCompatActivity {
         setContentView(R.layout.activity_demineur);
 
         tableauDemineur = findViewById(R.id.tableauDemineur);
+        listeMine = new ArrayList<>();
     }
 
     //On utilise cette methode pour pouvoir obtenir la hauteur et la largeur du tableau.
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
+        GenererMines();
         GenererTableau(tableauDemineur.getHeight(), tableauDemineur.getWidth());
+    }
+
+    private void GenererMines()
+    {
+        for(int i =0 ; i < 5; i++)
+        {
+            int randomX = new Random().nextInt(9) + 1;
+            int randomY = new Random().nextInt(9) + 1;
+
+            Mine mine = new Mine(this, randomX, randomY);
+            listeMine.add(mine);
+        }
     }
 
     private void GenererTableau(int height, int width)
     {
-
-        for (int i = 0; i < 9; i++) {
+        for (int y = 1; y <= 9; y++) {
             TableRow rangee = new TableRow(this);
 
-            for (int o = 0; o < 9; o++) {
-                Button boutton = new Button(this);
-                rangee.addView(boutton,width/9,height/9);
-            }
+            for (int x = 1; x <= 9; x++) {
 
+                for(Mine mine : listeMine)
+                {
+                    if(mine.getPositionX() == x && mine.getPositionY() == y)
+                    {
+                        rangee.addView(mine,width/9,height/9);
+                    }
+                }
+                if(rangee.getVirtualChildCount() == x - 1)
+                {
+                    Button button = new Button(this);
+                    button.setText("0");
+                    rangee.addView(button,width/9,height/9);
+                }
+            }
             tableauDemineur.addView(rangee);
         }
     }
--- app/src/main/java/com/example/a1738253/tp1/Mine.java
+++ app/src/main/java/com/example/a1738253/tp1/Mine.java
@@ -1,5 +1,36 @@
 package com.example.a1738253.tp1;
 
-public class Mine {
+import android.content.Context;
 
+public class Mine extends android.support.v7.widget.AppCompatButton{
+
+    private int positionX;
+    private int positionY;
+    private boolean estDesarmer;
+
+    public Mine(Context context, int x, int y)
+    {
+        super(context);
+        this.setBackgroundTintList(getResources().getColorStateList(R.color.colorPrimary));
+        this.setText("1");
+        estDesarmer = false;
+        positionX = x;
+        positionY = y;
+    }
+
+    public boolean isEstDesarmer() {
+        return estDesarmer;
+    }
+
+    public int getPositionX() {
+        return positionX;
+    }
+
+    public int getPositionY() {
+        return positionY;
+    }
+
+    public void setEstDesarmer(boolean estDesarmer) {
+        this.estDesarmer = estDesarmer;
+    }
 }
